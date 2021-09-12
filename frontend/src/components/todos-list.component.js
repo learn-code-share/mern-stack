5f8dd8d3ede732b515c7100cd83ae0aca5743859
Jitan Gupta <gtangupta@gmail.com>
2021-09-13 00:23:58 +0530

    added finish option on list

--- frontend/src/components/todos-list.component.js
+++ frontend/src/components/todos-list.component.js
@@ -8,7 +8,7 @@ const Todo = (props) => (
     <td className={props.todo.completed ? 'completed' : ''}>{props.todo.description}</td>
     <td className={props.todo.completed ? 'completed' : ''}>{props.todo.priority}</td>
     <td>
-      {/* <button type="button" onClick={() => props.completed(props.todo)} class="btn btn-primary btn-sm mx-1">Finish</button> */}
+      <button type="button" onClick={() => props.finished(props.todo)} className="btn btn-primary btn-sm mx-1">Finish</button>
       <Link className="btn btn-sm btn-secondary" to={"/edit/" + props.todo._id}>Edit</Link>
     </td>
   </tr>
@@ -34,12 +34,19 @@ export default class TodosList extends Component {
   }
 
   finishTask(task){
-    console.log(task);
+    task.completed = true;
+    axios
+      .put("http://localhost:4000/todos/" + task._id, task)
+      .then((res) => {
+        if (res.status === 200) {
+          this.props.history.push("/");
+        }
+      });
   }
 
   todoList() {
-    return this.state.todos.map(function (todo, index) {
-      return <Todo todo={todo} key={index} completed={(task) => this.finishTask(task)} />;
+    return this.state.todos.map((todo, index) => {
+      return <Todo todo={todo} key={index} finished={(task) => this.finishTask(task)} />;
     });
   }
 
